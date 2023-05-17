f553db0196ae849e42a2ad474dfd39478db09126
Ivan Pisquiy <pisquiy22@gmail.com>
2023-05-17 15:07:38 -0600

    Fixing links on footer

--- v4/index.php
+++ v4/index.php
@@ -734,6 +734,7 @@
         <div class="container">
             <div class="solar-footer-content">
                 <div class="row">
+                    <div class="footer-menu">
                         <div id="footer" class="form-group">
                             <p id="tcpa_label"> Copyright&copy; 2023&nbsp;poweredbysolar.energy.&nbsp;All Rights Reserved &nbsp;<a href="privacy.php" target="_new">Privacy policy</a> || <a href="terms.php" target="_new">Terms And Conditions</a> </p>
                         </div>
